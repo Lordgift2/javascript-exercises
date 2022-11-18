35a70c931f287d700969f62cccbc98eceaeba0c5
lordgift2 <adewunmimathew@gmail.com>
2022-11-18 21:58:03 +0100

    aim is to create an fuction that remove element from an arrey

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
 
+const removeFromArray = function removefromArray(a,b,c,d){
+    return arguments[0],arguments[1],arguments[3],arguments[4];
+    removeFromArray(1,2,3,4);
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
