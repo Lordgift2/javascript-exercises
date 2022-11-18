7332efade87aa9ab309e605a612fa0eae77c5b75
lordgift2 <adewunmimathew@gmail.com>
2022-11-18 21:42:35 +0100

    the SumAll.spec.js file was eddited to create a loop fuction and sum its variable

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+let sum=0;
+const sumAll = function Sum() {
+    for(let i=-10;i<=[90,1]; i++){
+        sum=sum+i;
+    }
+    Result=sum;
+    if(sum=URIError){Result="ERROR";}
+    return Result
 };
 
 // Do not edit below this line
