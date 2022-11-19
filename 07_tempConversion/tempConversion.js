1d6584053c983656c4889ce3a8df844c7372084b
lordgift2 <adewunmimathew@gmail.com>
2022-11-19 18:31:48 +0100

    the file is modified to create a temprature formulae function and also learn to use Maths.round()/10 to round of to 1 decimal place

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,21 @@
-const ftoc = function() {
+
+const ftoc = function ftoc() {
+  let F=-100
+  const subt=(F-32);
+  let C=subt*0.555555556;
+  if (C===0){return C}
+  else {return Math.round(C*10)/10};
+
 
 };
 
-const ctof = function() {
-
+const ctof = function Fehrant() {
+  let C=-10
+  let step1=9*C;
+  let step2=step1/5;
+let F=32+step2;
+if (F===0){return F}
+else {return Math.round(F*10)/10}
 };
 
 // Do not edit below this line
